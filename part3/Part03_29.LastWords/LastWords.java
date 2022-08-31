26375a209db07804765dd5f2f461b1d4c190b558
David Melumian <dmelumian@gmail.com>
2022-08-31 07:55:21 +0300

    twelfth  commit

---
+++ part3/Part03_29.LastWords/LastWords.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class LastWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String input = scanner.nextLine();
+            if (input.equals("")){
+                break;
+            }
+            String [] parts = input.split(" ");
+            int index = parts.length -1;
+            System.out.println(parts[index]);
+        }
+    }
+}
